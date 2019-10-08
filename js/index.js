3417475e2b6cad4e1b49f60bd16d666c4b8e5050
Michelle Tinajero <elletinajero11@gmail.com>
2019-10-08 17:23:18 -0500

    added to DOM II project

--- js/index.js
+++ js/index.js
@@ -1,1 +1,104 @@
 // Your code goes here
+//MOUSEOVER EVENT//
+let firstEvent = document.querySelector("header .nav");
+console.log(firstEvent);
+
+firstEvent.addEventListener("mouseover", function(event){
+    event.target.style.color = "orange";
+    setTimeout(function () {
+        event.target.style.color = "";
+    },500);
+}, false);
+
+//form event//
+
+function logSubmit(event) {
+    log.textContent = `Thanks for keeping us alive! Time stamp: ${event.timeStamp}`;
+    event.preventDefault();
+  }
+  const form = document.getElementById('form');
+  const log = document.getElementById('log');
+  form.addEventListener('submit', logSubmit);
+
+//button event//
+
+const button = document.querySelector(".btn");
+
+button.addEventListener('click', event => {
+  button.innerHTML = `Click count: ${event.detail}`;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------still need more understanding------------------------------------------------------------------//
+//DRAG EVENT//
+var dragged;
+
+/* events fired on the draggable target */
+document.addEventListener("drag", function(event) {
+
+}, false);
+
+document.addEventListener("dragstart", function(event) {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  // make it half transparent
+  event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function(event) {
+  // reset the transparency
+  event.target.style.opacity = "";
+}, false);
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function(event) {
+  // prevent default to allow drop
+  event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function(event) {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "purple";
+  }
+
+}, false);
+
+document.addEventListener("dragleave", function(event) {
+  // reset background of potential drop target when the draggable element leaves it
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+  }
+
+}, false);
+
+document.addEventListener("drop", function(event) {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged elem to the selected drop target
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+    dragged.parentNode.removeChild( dragged );
+    event.target.appendChild( dragged );
+  }
+}, false);
+
